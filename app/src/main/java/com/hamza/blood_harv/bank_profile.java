ca84b09219c4034ef109438a08fa3a1944970c82
zain <mzainulabideen.fastian@gmail.com>
2021-10-20 23:14:18 +0500

    Added these: 1. donor_profile layout 2. bank_profile layout

---
+++ app/src/main/java/com/hamza/blood_harv/bank_profile.java
@@ -0,0 +1,14 @@
+package com.hamza.blood_harv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bank_profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bank_profile);
+    }
+}
